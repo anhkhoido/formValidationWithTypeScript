e090b7645d1934040e36c80f60eca3d1eb2934d0
anhkhoido <anhkhoido@gmail.com>
2020-12-08 17:15:41 -0500

    Augmented index.html and validation.ts.

--- validation.ts
+++ validation.ts
@@ -1,1 +1,5 @@
 const firstName = document.getElementById('firstname');
+const lastName = document.getElementById('lastName');
+
+const submitButton = document.getElementById('submitButton');
+const resetButton = document.getElementById('resetButton');
