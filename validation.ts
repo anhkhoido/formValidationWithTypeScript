fdf338728947d1a3c1dc605feaec68d6579def2e
anhkhoido <anhkhoido@gmail.com>
2020-12-08 17:19:33 -0500

    Adding more fields.

--- validation.ts
+++ validation.ts
@@ -1,5 +1,9 @@
 const firstName = document.getElementById('firstname');
-const lastName = document.getElementById('lastName');
+const lastName = document.getElementById('lastname');
 
 const submitButton = document.getElementById('submitButton');
 const resetButton = document.getElementById('resetButton');
+
+submitButton.addEventListener("click", function() {
+    console.log("You clicked the submit button.");
+});
