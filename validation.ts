4b072d8b476543523fd53fe8faa8eb0ed4ff5b3b
anhkhoido <anhkhoido@gmail.com>
2020-12-08 17:22:58 -0500

    Re-arrange the form.

--- validation.ts
+++ validation.ts
@@ -2,7 +2,6 @@ const firstName = document.getElementById('firstname');
 const lastName = document.getElementById('lastname');
 
 const submitButton = document.getElementById('submitButton');
-const resetButton = document.getElementById('resetButton');
 
 submitButton.addEventListener("click", function() {
     console.log("You clicked the submit button.");
