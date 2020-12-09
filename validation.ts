195b6e7d3432d8b7b377665d06685ea5d3a2c319
anhkhoido <anhkhoido@gmail.com>
2020-12-08 19:33:39 -0500

    Added functions to validate fields in the form.

--- validation.ts
+++ validation.ts
@@ -1,13 +1,14 @@
 const firstName = document.getElementById('firstname') as HTMLInputElement;
 const lastName = document.getElementById('lastname') as HTMLInputElement;
+const emailAddress = document.getElementById('email') as HTMLInputElement;
+
 const addressline1 = document.getElementById('addressline1') as HTMLInputElement;
+const postalCode = document.getElementById('postalcode') as HTMLInputElement;
 
 const submitButton = document.getElementById('submitButton');
 
-submitButton.addEventListener("click", function() {
-    validateFirstOrLastName(firstName.value);
-    validateFirstOrLastName(lastName.value);
-    validateAddress(addressline1.value);
+submitButton.addEventListener("click", function() : boolean {
+    return validateFirstOrLastName(firstName.value) && validateFirstOrLastName(lastName.value) && validateAddress(addressline1.value) && validatePostalCode(postalCode.value);
 });
 
 function validateFirstOrLastName(name : string) : boolean {
@@ -15,5 +16,13 @@ function validateFirstOrLastName(name : string) : boolean {
 }
 
 function validateAddress(line : string) : boolean {
+    return false;
+}
+
+function validatePostalCode(postalCode : string) : boolean {
+    return false;
+}
+
+function validateEmailAddress(email : string) : boolean {
     return false;
 }
