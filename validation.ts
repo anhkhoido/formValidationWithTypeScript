c590e1c6fb2323224b772a60f6ae9bb46f610693
anhkhoido <anhkhoido@gmail.com>
2023-01-20 21:38:11 -0500

    Added function updating education fieldset.

--- validation.ts
+++ validation.ts
@@ -9,6 +9,7 @@ const province = document.getElementById('province') as HTMLInputElement;
 const postalCode = document.getElementById('postalcode') as HTMLInputElement;
 
 const submitButton = document.getElementById('submitButton') as HTMLButtonElement;
+const addEducationButton = document.getElementById('addEducationButtonId') as HTMLButtonElement;
 
 const firstLastNameRegexp : RegExp = /[A-Za-z -'\xC9\xE9\xE8\xC8\xC0\xE0\xC7\xE7\xCA\xEA\xF9\xD9\u0110\u0111\xC1\xE1\xC3\xE3\u1EA2\u1EA3\u1EA0\u1EA1\xC2\xE2\u1EA4\u1EA5\u1EA6\u1EA7\u1EAA\u1EAB\u1EA8\u1EA9\u1EAC\u1EAD\u0102\u0103\u1EAE\u1EAF\u1EB0\u1EB1\u1EB4\u1EB5\u1EB2\u1EB3\u1EB6\u1EB7\xCB\xEB\u1EBC\u1EBA\u1EBB\u1EB8\u1EB9\u1EBE\u1EBF\u1EC0\u1EC1\u1EC4\u1EC5\u1EC2\u1EC3\u1EC6\u1EC7\xCD\xED\xCC\xEC\u0128\u0129\u1EC8\u1EC9\u1ECA\u1ECB\xD1\xF1\xD3\xF3\xD2\xF2\xD5\xF5\u1ECE\u1ECF\u1ECC\u1ECD\u1ED0\u1ED1\u1ED2\u1ED3\u1ED6\u1ED7\u1ED4\u1ED5\u1ED8\u1ED9\u01A0\u01A1\u1EDA\u1EDB\u1EDC\u1EDD\u1EE0\u1EE1\u1EDE\u1EDF\u1EE2\u1EE3\xDC\xFC\xDA\xFA\u0168\u0169\u1EE6\u1EE7\u1EE4\u1EE5\u01AF\u01B0\u1EE8\u1EE9\u1EEA\u1EEB\u1EEE\u1EEF\u1EEC\u1EED\u1EF0\u1EF1\xDD\xFD\u1EF2\u1EF3\u1EF8\u1EF9\u1EF6\u1EF7\u1EF4\u1EF5]{2,}/i;
 const addressRegularExpression = new RegExp("");
@@ -16,6 +17,7 @@ const phoneNumberRegExp : RegExp = /[0-9]{10}|[0-9]{3}\-[0-9]{3}\-[0-9]{4}|\x28{
 const postalCodeRegExp : RegExp = /[A-Za-z]{1}\d{1}[A-Za-z]{1} {1}\d{1}[A-Za-z]{1}\d{1}|[A-Za-z]{1}\d{1}[A-Za-z]{1}\d{1}[A-Za-z]{1}\d{1}/i;
 
 submitButton.addEventListener("click", validateApplicationForm, false);
+addEducationButton.addEventListener("click", addEducation, false);
 
 function validateApplicationForm() : boolean {
     const firstNameValid : boolean = validateFirstOrLastName(firstName.value, firstName);
@@ -69,4 +71,38 @@ function validatePostalCode(postal_code : string) : boolean {
     }
     postalCode.style.backgroundColor = "#cc6666";
     return false;
+}
+
+function addEducation() : void {
+    const fieldsetEducation = document.getElementById('educationContainerId') as HTMLElement;
+    
+    var breakLine = document.createElement('br');
+
+    var divBox = document.createElement('div');
+    divBox.style.display = 'block';
+    divBox.style.width = '70%';
+    divBox.style.borderColor = '#000';
+    divBox.style.border = '5px solid #000';
+    divBox.style.margin = '5px';
+    
+    var establishmentLegend = document.createElement('label');
+    establishmentLegend.innerText = 'Establishment';
+
+    var establishmentInputField = document.createElement('input');
+    establishmentInputField.type = 'text';
+
+
+    var programLegend = document.createElement('label');
+    programLegend.innerText = 'Program';
+
+    var programInputField = document.createElement('input');
+    programInputField.type = 'text';
+
+    divBox.appendChild(establishmentLegend);
+    divBox.appendChild(establishmentInputField);
+    divBox.appendChild(breakLine);
+    divBox.appendChild(programLegend);
+    divBox.appendChild(programInputField);
+
+    fieldsetEducation.appendChild(divBox);
 }
