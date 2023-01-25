5283d8f18b5a3c64e422ffba50a7062f6e8ff1f1
anhkhoido <anhkhoido@gmail.com>
2023-01-25 15:38:02 -0500

    Added class for province

---
+++ model/geography/province.ts
@@ -0,0 +1,8 @@
+class Province {
+    name : string;
+    abbreviation : string;
+    public constructor(name : string, abbreviation : string) {
+        this.name = name;
+        this.abbreviation = abbreviation;
+    }
+}
