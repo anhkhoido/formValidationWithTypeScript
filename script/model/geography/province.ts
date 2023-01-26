c548d6230784122cacd6715124df6a50f9e2a4a7
anhkhoido <anhkhoido@gmail.com>
2023-01-26 18:05:54 -0500

    Re-wrote validation.ts

--- script/model/geography/province.ts
+++
@@ -1,6 +0,0 @@
-export class Province {
-    constructor(
-        public name : string, 
-        public abbreviation : string
-    ) {}
-}
--- script/validation.ts
+++ script/validation.ts
@@ -1,5 +1,3 @@
-import { Province } from "./model/geography/province";
-
 const firstName = document.getElementById('firstname') as HTMLInputElement;
 const lastName = document.getElementById('lastname') as HTMLInputElement;
 const emailAddress = document.getElementById('email') as HTMLInputElement;
@@ -14,6 +12,10 @@ const submitButton = document.getElementById('submitButton') as HTMLButtonElemen
 const addEducationButton = document.getElementById('addEducationButtonId') as HTMLButtonElement;
 const addProfessionalExperienceButton = document.getElementById('addProfessionalExperienceButtonId') as HTMLButtonElement;
 
+class Province {
+    constructor(public name : string, public abbreviation : string) {}
+}
+
 let provincesSelectTag = document.getElementById('provinceSelectId') as HTMLSelectElement;
 
 const firstLastNameRegexp : RegExp = /[A-Za-z -'\xC9\xE9\xE8\xC8\xC0\xE0\xC7\xE7\xCA\xEA\xF9\xD9\u0110\u0111\xC1\xE1\xC3\xE3\u1EA2\u1EA3\u1EA0\u1EA1\xC2\xE2\u1EA4\u1EA5\u1EA6\u1EA7\u1EAA\u1EAB\u1EA8\u1EA9\u1EAC\u1EAD\u0102\u0103\u1EAE\u1EAF\u1EB0\u1EB1\u1EB4\u1EB5\u1EB2\u1EB3\u1EB6\u1EB7\xCB\xEB\u1EBC\u1EBA\u1EBB\u1EB8\u1EB9\u1EBE\u1EBF\u1EC0\u1EC1\u1EC4\u1EC5\u1EC2\u1EC3\u1EC6\u1EC7\xCD\xED\xCC\xEC\u0128\u0129\u1EC8\u1EC9\u1ECA\u1ECB\xD1\xF1\xD3\xF3\xD2\xF2\xD5\xF5\u1ECE\u1ECF\u1ECC\u1ECD\u1ED0\u1ED1\u1ED2\u1ED3\u1ED6\u1ED7\u1ED4\u1ED5\u1ED8\u1ED9\u01A0\u01A1\u1EDA\u1EDB\u1EDC\u1EDD\u1EE0\u1EE1\u1EDE\u1EDF\u1EE2\u1EE3\xDC\xFC\xDA\xFA\u0168\u0169\u1EE6\u1EE7\u1EE4\u1EE5\u01AF\u01B0\u1EE8\u1EE9\u1EEA\u1EEB\u1EEE\u1EEF\u1EEC\u1EED\u1EF0\u1EF1\xDD\xFD\u1EF2\u1EF3\u1EF8\u1EF9\u1EF6\u1EF7\u1EF4\u1EF5]{2,}/i;
@@ -25,7 +27,7 @@ submitButton.addEventListener("click", validateApplicationForm, true);
 addEducationButton.addEventListener("click", addEducation, false);
 addProfessionalExperienceButton.addEventListener("click", addProfessionalExperience, false);
 
-populateProvince();
+populateProvinces();
 
 function validateApplicationForm() : boolean {
     const firstNameValid : boolean = validateFirstOrLastName(firstName.value, firstName);
@@ -44,17 +46,31 @@ function validateApplicationForm() : boolean {
     return  result;
 }
 
-
-function populateProvince() : void {
-    var myProvinces : Province[] =[
-            new Province("Ontario", "ON"),
-            new Province("Quebec", "QC")
+function populateProvinces() : void {
+    var arrayOfProvinces : Province[] = [
+        new Province("British Columbia", "BC"),
+        new Province("Alberta", "AB"),
+        new Province("Saskatchewan", "SK"),
+        new Province("Manitoba", "MB"),
+        new Province("Ontario", "ON"),
+        new Province("Quebec", "QC"),
+        new Province("New Brunswick", "NB"),
+        new Province("Nova Scotia", "NS"),
+        new Province("Prince Edward Island", "PEI"),
+        new Province("Newfoundland and Labrador", "NL"),
+        new Province("Yukon", "YK"),
+        new Province("Nunavut", "NT"),
+        new Province("Northwest Territories", "NWT")
     ];
-    myProvinces.forEach(province => {
-        var option = document.createElement('option');
-        option.innerText = province.name;
-        option.value = province.abbreviation;
-        provincesSelectTag.appendChild(option);
+    var emptyOptionForSelectTag = document.createElement('option') as HTMLOptionElement;
+    emptyOptionForSelectTag.value = "";
+    provincesSelectTag.appendChild(emptyOptionForSelectTag);
+
+    arrayOfProvinces.forEach(province => {
+        var childProvince = document.createElement('option') as HTMLOptionElement;
+        childProvince.innerText = province.name;
+        childProvince.value = province.abbreviation;
+        provincesSelectTag.appendChild(childProvince);
     });
 }
 
